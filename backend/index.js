76c4e0245d4b3add75cb3a2dcc0d6681dee42924
vignesh-vic <vignesha010@gmail.com>
2024-07-18 10:35:22 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 dotenv.config();
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://blog-website-r9yv.vercel.app'
+    'https://blog-website-client-pi.vercel.app'
 ];
 
 const corsOptions = {
